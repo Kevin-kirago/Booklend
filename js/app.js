cb80fa5645482731e4985e454130ac15e8e770f4
Kevin <tkevinkiragu@gmail.com>
2019-08-26 00:53:20 +0300

    Booklending Application Implementing the add book logic

--- js/app.js
+++ js/app.js
@@ -39,19 +39,53 @@ var Model = (function() {
 
 var View = (function() {
 	//
-	// 2. Get input values
 
 	return {
+		// 1. Get input values
 		getInput: function() {
 			return {
 				title: document.getElementById("title").value,
 				author: document.getElementById("author").value,
 				isbn: document.getElementById("isbn").value
 			};
+		},
+
+		// 2. Add the new item to the view
+
+		addBookItem: function(obj) {
+			var html, element;
+
+			element = ".book__list";
+			html = `
+			<div class="item">
+				<div class="item__content">
+					<div class="item__title"><span class="item__label">Name:</span>${obj.title}</div>
+					<div class="item__author"><span class="item__label">Author:</span>${obj.author}</div>
+					<div class="item__number"><span class="item__label">ISBN&nbsp;:</span>${obj.isbn}</div>
+				</div>
+				<div class="item__delete">
+					<button class="item__delete--btn">
+						<i class="ion-ios-close-outline"></i>
+					</button>
+				</div>
+			</div>`;
+
+			// Insert the Html into the Dom
+			document.querySelector(element).insertAdjacentHTML("beforeend", html);
+		},
+
+		clearInputFields: function() {
+			var field;
+
+			field = document.getElementById("title");
+
+			field.value = "";
+			document.getElementById("author").value = "";
+			document.getElementById("isbn").value = "";
+
+			field.focus();
 		}
 	};
-
-	// 4. Add the new item to the view
 })();
 
 // ///////////////////////////////////////////////////////////////
@@ -82,11 +116,12 @@ var Controller = (function(md, ui) {
 		if (input.title !== "" && input.author !== "" && !isNaN(input.isbn)) {
 			// 3. Add item to the model controller
 			newBookItem = md.addBookItem(input.title, input.author, input.isbn);
-			console.log(md.testing());
 
 			// 4. Add item to the view controller
+			ui.addBookItem(newBookItem);
 
 			// 5. clear the input fields
+			ui.clearInputFields();
 		}
 	};
 
